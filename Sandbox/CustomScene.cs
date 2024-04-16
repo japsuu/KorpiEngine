36970b116bd2b9c0f669f001305f262239ac8672
Jasper <japsu.honkasalo@gmail.com>
2024-04-16 13:57:37 +0300

    Sandbox scene adjustments

--- Sandbox/CustomScene.cs
+++ Sandbox/CustomScene.cs
@@ -19,7 +19,7 @@ internal class CustomScene : Scene
     protected override void Load()
     {
         _blueBoxEntity = CreatePrimitive(PrimitiveType.Quad, "Blue Box");
-        _blueBoxEntity.Transform.Position = new Vector3(0, 5, 0);
+        _blueBoxEntity.Transform.Position = new Vector3(0, 3, 0);
             
         StandardMaterial3D blueMaterial = (StandardMaterial3D)_blueBoxEntity.GetComponent<MeshRenderer>()!.Material!;
         blueMaterial.Color = Color.Blue;
@@ -45,14 +45,13 @@ internal class CustomScene : Scene
         // Rotate the entity
         const float rotSpeedY = 15f;
         const float rotSpeedZ = 30f;
-        Vector3 eulerAngles = _blueBoxEntity.Transform.EulerAngles;
-        Vector3 newEulerAngles = new Vector3(eulerAngles.X, eulerAngles.Y + rotSpeedY * Time.DeltaTime, eulerAngles.Z + rotSpeedZ * Time.DeltaTime);
+        Vector3 newEulerAngles = new Vector3(0, rotSpeedY * Time.DeltaTime, rotSpeedZ * Time.DeltaTime);
         _blueBoxEntity.Transform.Rotate(newEulerAngles);
-            
+        
         // Move the entity
         const float moveSpeed = 0.1f;
         _blueBoxEntity.Transform.Translate(new Vector3(1f, 0f, 0f) * moveSpeed * Time.DeltaTime);
-            
-        Console.WriteLine($"Blue Box position: {_blueBoxEntity.Transform.EulerAngles:F2}");
+        
+        // Console.WriteLine($"Blue Box position: {_blueBoxEntity.Transform.Position:F2}");
     }
 }
