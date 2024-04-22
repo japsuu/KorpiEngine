ac14f20797da80a5d874e6a7b45636249687314d
Jasper <japsu.honkasalo@gmail.com>
2024-04-22 14:40:51 +0300

    Add TextureImageFormat XML docs

--- src/KorpiEngine.Runtime/Core/Rendering/Primitives/TextureImageFormat.cs
+++ src/KorpiEngine.Runtime/Core/Rendering/Primitives/TextureImageFormat.cs
@@ -2,26 +2,134 @@
 
 public enum TextureImageFormat
 {
-    Color4B,
-    UnsignedShort4,
+    #region COMMON RGBA formats
 
-    Float,
-    Float2,
-    Float3,
-    Float4,
-    Depth16,
-    Depth24,
-    Depth32F,
+    /// <summary>
+    /// Red, Green, Blue, and Alpha.
+    /// Unsigned 8-bit per channel.
+    /// </summary>
+    RGBA_8,
+    
+    /// <summary>
+    /// Red, Green, Blue, and Alpha.
+    /// Unsigned 16-bit per channel.
+    /// </summary>
+    RGBA_16,
 
-    Int,
-    Int2,
-    Int3,
-    Int4,
+    #endregion
 
-    UnsignedInt,
-    UnsignedInt2,
-    UnsignedInt3,
-    UnsignedInt4,
 
-    Depth24Stencil8
+    #region FLOAT formats
+
+    /// <summary>
+    /// Red.
+    /// 32-bit float per channel.
+    /// </summary>
+    R_32_F,
+    
+    /// <summary>
+    /// Red and Green.
+    /// 32-bit float per channel.
+    /// </summary>
+    RG_32_F,
+    
+    /// <summary>
+    /// Red, Green, and Blue.
+    /// 32-bit float per channel.
+    /// </summary>
+    RGB_32_F,
+    
+    /// <summary>
+    /// Red, Green, Blue, and Alpha.
+    /// 32-bit float per channel.
+    /// </summary>
+    RGBA_32_F,
+
+    #endregion
+
+
+    #region INTEGER formats
+
+    /// <summary>
+    /// Red.
+    /// 32-bit integer per channel.
+    /// </summary>
+    R_32_I,
+    
+    /// <summary>
+    /// Red and Green.
+    /// 32-bit integer per channel.
+    /// </summary>
+    RG_32_I,
+    
+    /// <summary>
+    /// Red, Green, and Blue.
+    /// 32-bit integer per channel.
+    /// </summary>
+    RGB_32_I,
+    
+    /// <summary>
+    /// Red, Green, Blue, and Alpha.
+    /// 32-bit integer per channel.
+    /// </summary>
+    RGBA_32_I,
+
+    #endregion
+
+
+    #region UNSIGNED INTEGER formats
+
+    /// <summary>
+    /// Red.
+    /// 32-bit unsigned integer per channel.
+    /// </summary>
+    R_32_UI,
+    
+    /// <summary>
+    /// Red and Green.
+    /// 32-bit unsigned integer per channel.
+    /// </summary>
+    RG_32_UI,
+    
+    /// <summary>
+    /// Red, Green, and Blue.
+    /// 32-bit unsigned integer per channel.
+    /// </summary>
+    RGB_32_UI,
+    
+    /// <summary>
+    /// Red, Green, Blue, and Alpha.
+    /// 32-bit unsigned integer per channel.
+    /// </summary>
+    RGBA_32_UI,
+
+    #endregion
+
+
+    #region DEPTH formats
+
+    /// <summary>
+    /// Single-channel depth.
+    /// 16-bits per channel.
+    /// </summary>
+    DEPTH_16,
+    
+    /// <summary>
+    /// Single-channel depth.
+    /// 24-bits per channel.
+    /// </summary>
+    DEPTH_24,
+    
+    /// <summary>
+    /// Single-channel depth.
+    /// 32-bit float per channel.
+    /// </summary>
+    DEPTH_32_F,
+
+    /// <summary>
+    /// 24-bit depth and 8-bit stencil.
+    /// </summary>
+    DEPTH_24_STENCIL_8
+
+    #endregion
 }
