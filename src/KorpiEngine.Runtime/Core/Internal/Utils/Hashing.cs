2286e134e4ba81cfdff316d8f8ca0165a5b41c3c
Jasper <japsu.honkasalo@gmail.com>
2024-05-13 11:26:57 +0300

    Add HashSet hashing util

---
+++ src/KorpiEngine.Runtime/Core/Internal/Utils/Hashing.cs
@@ -0,0 +1,14 @@
+﻿namespace KorpiEngine.Core.Internal.Utils;
+
+public static class Hashing
+{
+    public static int GetHashCode<T>(HashSet<T> hashSet)
+    {
+        int hashCode = 0;
+        foreach (T item in hashSet)
+        {
+            hashCode ^= HashCode.Combine(hashSet.Comparer.GetHashCode(item!));
+        }
+        return hashCode;
+    }
+}
