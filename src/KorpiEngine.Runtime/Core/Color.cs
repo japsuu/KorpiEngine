4d810392b8cdeecd03989d69c1789b34e3b837f6
Jasper <japsu.honkasalo@gmail.com>
2024-06-13 08:13:52 +0300

    Add Color implicit conversions to Vec4s

--- src/KorpiEngine.Runtime/Core/Color.cs
+++ src/KorpiEngine.Runtime/Core/Color.cs
@@ -1,4 +1,6 @@
-﻿namespace KorpiEngine.Core;
+﻿using KorpiEngine.Core.API;
+
+namespace KorpiEngine.Core;
 
 public readonly struct Color
 {
@@ -17,16 +19,16 @@ public readonly struct Color
     }
     
     
-    public static Color White => new Color(1, 1, 1, 1);
-    public static Color Black => new Color(0, 0, 0, 1);
-    public static Color Gray => new Color(0.5f, 0.5f, 0.5f, 1);
-    public static Color Red => new Color(1, 0, 0, 1);
-    public static Color Green => new Color(0, 1, 0, 1);
-    public static Color Blue => new Color(0, 0, 1, 1);
-    public static Color Yellow => new Color(1, 1, 0, 1);
-    public static Color Cyan => new Color(0, 1, 1, 1);
-    public static Color Magenta => new Color(1, 0, 1, 1);
-    public static Color Transparent => new Color(0, 0, 0, 0);
+    public static Color White => new(1, 1, 1, 1);
+    public static Color Black => new(0, 0, 0, 1);
+    public static Color Gray => new(0.5f, 0.5f, 0.5f, 1);
+    public static Color Red => new(1, 0, 0, 1);
+    public static Color Green => new(0, 1, 0, 1);
+    public static Color Blue => new(0, 0, 1, 1);
+    public static Color Yellow => new(1, 1, 0, 1);
+    public static Color Cyan => new(0, 1, 1, 1);
+    public static Color Magenta => new(1, 0, 1, 1);
+    public static Color Transparent => new(0, 0, 0, 0);
 
 
     public void Deconstruct(out float r, out float g, out float b, out float a)
@@ -36,4 +38,10 @@ public readonly struct Color
         b = B;
         a = A;
     }
+    
+    public static implicit operator Vector4(Color c) => new(c.R, c.G, c.B, c.A);
+    public static implicit operator System.Numerics.Vector4(Color c) => new(c.R, c.G, c.B, c.A);
+
+    public static implicit operator Color(Vector4 v) => new((float)v.X, (float)v.Y, (float)v.Z, (float)v.W);
+    public static implicit operator Color(System.Numerics.Vector4 v) => new(v.X, v.Y, v.Z, v.W);
 }
