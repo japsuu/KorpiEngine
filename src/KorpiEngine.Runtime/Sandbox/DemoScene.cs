7f20aef2bfa8296f49f0fcba6de80ae261264ea6
Jasper <japsu.honkasalo@gmail.com>
2024-06-27 10:25:28 +0300

    Add demo scene oscillation offset

--- src/KorpiEngine.Runtime/Sandbox/DemoScene.cs
+++ src/KorpiEngine.Runtime/Sandbox/DemoScene.cs
@@ -67,12 +67,22 @@ internal class DemoOscillate : EntityComponent
     private const float OSCILLATION_SPEED = 1f;
     private const float OSCILLATION_HEIGHT = 2f;
 
+    private double _oscillationOffset;
+
+    
+    protected override void OnStart()
+    {
+        // Generate a random offset in the 0-1 range to make the oscillation unique for each entity
+        _oscillationOffset = Random.Range(0f, 1f);
+    }
+
 
     protected override void OnUpdate()
     {
         // Oscillate the entity up and down
-        double oscillation = Maths.Sin(Time.TotalTime * OSCILLATION_SPEED) * OSCILLATION_HEIGHT;
-        Transform.Position = new Vector3(Transform.Position.X, oscillation, Transform.Position.Z);
+        double time = Time.TotalTime + _oscillationOffset;
+        double height = Math.Sin(time * OSCILLATION_SPEED) * OSCILLATION_HEIGHT;
+        Transform.Position = new Vector3(Transform.Position.X, height, Transform.Position.Z);
     }
 }
 
@@ -104,7 +114,6 @@ internal class DemoFreeCam : EntityComponent
 
     protected override void OnUpdate()
     {
-        Console.WriteLine("FreeCam Update");
         UpdateCursorLock();
 
         UpdatePosition();
