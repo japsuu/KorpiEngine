cced23c18b99d0d4ab5e7719ecd195b4f3d0f38c
Jasper <japsu.honkasalo@gmail.com>
2024-07-26 12:55:34 +0300

    WIP Mesh.CreateSphere fix

--- src/KorpiEngine.Runtime/Core/API/Rendering/Mesh.cs
+++ src/KorpiEngine.Runtime/Core/API/Rendering/Mesh.cs
@@ -817,43 +817,104 @@ public sealed class Mesh : Resource //TODO: Implement MeshData class to hide som
 #warning TODO: Fix Mesh.CreateSphere
     public static Mesh CreateSphere(float radius, int rings, int slices)
     {
-        List<System.Numerics.Vector3> vertices = [];
-        List<System.Numerics.Vector2> uvs = [];
-        List<int> indices = [];
+        // Longitude |||
+        int nbLong = 24;
 
-        for (int i = 0; i <= rings; i++)
+        // Latitude ---
+        int nbLat = 16;
+
+
+        #region Vertices
+
+        Vector3[] vertices = new Vector3[(nbLong + 1) * nbLat + 2];
+        float _pi = Mathf.PI;
+        float _2pi = _pi * 2f;
+
+        vertices[0] = Vector3.up * radius;
+        for (int lat = 0; lat < nbLat; lat++)
         {
-            float v = 1 - (float)i / rings;
-            float phi = v * MathF.PI;
+            float a1 = _pi * (float)(lat + 1) / (nbLat + 1);
+            float sin1 = Mathf.Sin(a1);
+            float cos1 = Mathf.Cos(a1);
 
-            for (int j = 0; j <= slices; j++)
+            for (int lon = 0; lon <= nbLong; lon++)
             {
-                float u = (float)j / slices;
-                float theta = u * MathF.PI * 2;
+                float a2 = _2pi * (float)(lon == nbLong ? 0 : lon) / nbLong;
+                float sin2 = Mathf.Sin(a2);
+                float cos2 = Mathf.Cos(a2);
 
-                float x = MathF.Sin(phi) * MathF.Cos(theta);
-                float y = MathF.Cos(phi);
-                float z = MathF.Sin(phi) * MathF.Sin(theta);
-
-                vertices.Add(new System.Numerics.Vector3(x, y, z) * radius);
-                uvs.Add(new System.Numerics.Vector2(u, v));
+                vertices[lon + lat * (nbLong + 1) + 1] = new Vector3(sin1 * cos2, cos1, sin1 * sin2) * radius;
             }
         }
 
-        for (int i = 0; i < rings; i++)
-        for (int j = 0; j < slices; j++)
+        vertices[vertices.Length - 1] = Vector3.up * -radius;
+
+        #endregion
+
+
+        #region Normales
+
+        Vector3[] normales = new Vector3[vertices.Length];
+        for (int n = 0; n < vertices.Length; n++)
+            normales[n] = vertices[n].normalized;
+
+        #endregion
+
+
+        #region UVs
+
+        Vector2[] uvs = new Vector2[vertices.Length];
+        uvs[0] = Vector2.up;
+        uvs[uvs.Length - 1] = Vector2.zero;
+        for (int lat = 0; lat < nbLat; lat++)
+        for (int lon = 0; lon <= nbLong; lon++)
+            uvs[lon + lat * (nbLong + 1) + 1] = new Vector2((float)lon / nbLong, 1f - (float)(lat + 1) / (nbLat + 1));
+
+        #endregion
+
+
+        #region Triangles
+
+        int nbFaces = vertices.Length;
+        int nbTriangles = nbFaces * 2;
+        int nbIndexes = nbTriangles * 3;
+        int[] triangles = new int[nbIndexes];
+
+        //Top Cap
+        int i = 0;
+        for (int lon = 0; lon < nbLong; lon++)
         {
-            int a = i * (slices + 1) + j;
-            int b = a + slices + 1;
-
-            indices.Add(a);
-            indices.Add(b);
-            indices.Add(a + 1);
-
-            indices.Add(b);
-            indices.Add(b + 1);
-            indices.Add(a + 1);
+            triangles[i++] = lon + 2;
+            triangles[i++] = lon + 1;
+            triangles[i++] = 0;
         }
+
+        //Middle
+        for (int lat = 0; lat < nbLat - 1; lat++)
+        for (int lon = 0; lon < nbLong; lon++)
+        {
+            int current = lon + lat * (nbLong + 1) + 1;
+            int next = current + nbLong + 1;
+
+            triangles[i++] = current;
+            triangles[i++] = current + 1;
+            triangles[i++] = next + 1;
+
+            triangles[i++] = current;
+            triangles[i++] = next + 1;
+            triangles[i++] = next;
+        }
+
+        //Bottom Cap
+        for (int lon = 0; lon < nbLong; lon++)
+        {
+            triangles[i++] = vertices.Length - 1;
+            triangles[i++] = vertices.Length - (lon + 2) - 1;
+            triangles[i++] = vertices.Length - (lon + 1) - 1;
+        }
+
+        #endregion
+
 
         Mesh mesh = new();
         mesh.SetVertexPositions(vertices.ToArray());
