ef60231b5b0b893b9c19bf1dd9d5d08d50fedc07
Jasper <japsu.honkasalo@gmail.com>
2024-08-30 09:57:38 +0300

    Add Vim.Math3D as an external maths library

---
+++ src/Math/IPoints.cs
@@ -0,0 +1,13 @@
+﻿namespace KorpiEngine;
+
+public interface IPoints
+{
+    int NumPoints { get; }
+    Vector3 GetPoint(int n);
+}
+
+public interface IPoints2D
+{
+    int NumPoints { get; }
+    Vector2 GetPoint(int n);
+}
---
+++ src/Math/LinqUtil.cs
@@ -0,0 +1,7 @@
+﻿namespace KorpiEngine;
+
+public static class LinqUtil
+{
+    public static AABox ToAABox(this IEnumerable<Vector3> self)
+        => AABox.Create(self);
+}
