fb4bba7f1fcf8342b338191e2f36dc0b8daa884d
Jasper <japsu.honkasalo@gmail.com>
2024-09-18 07:36:52 +0300

    Clean up Debug.Asset stacktrace

--- src/Core/Tools/Debug.cs
+++ src/Core/Tools/Debug.cs
@@ -5,16 +5,18 @@ namespace KorpiEngine.Tools;
 
 internal static class Debug
 {
+    [StackTraceHidden]
     [Conditional("TOOLS")]
     public static void Assert(bool condition, string message)
     {
         if (!condition)
-            throw new KorpiException($"Engine bug: {message}");
+            throw new KorpiException($"Assertion failed (Engine bug): {message}");
     }
     
     /// <summary>
     /// Throws an exception if the current thread is not the main thread.
     /// </summary>
+    [StackTraceHidden]
     [Conditional("TOOLS")]
     public static void AssertMainThread(bool shouldBeMainThread)
     {
